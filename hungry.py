98c44edd3353919e3969be5ed66f6c0c72bf2a95
Priyanka Ghosh <prynka.gsh@gmail.com>
2025-01-03 21:59:40 +0530

    master change

--- hungry.py
+++ hungry.py
@@ -2,6 +2,7 @@ hungry=input("Are you hungry?")
 if hungry == "yes":
     print("Eat Biriyani!!")
     print("Eat Pizza")
+    print("master change")
     print("eat snacks")
     print("eat samosa")
 else:
