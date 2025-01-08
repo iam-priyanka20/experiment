d139e68d8f896a8faed2697cdd2e704c1fb11a1e
Priyanka Ghosh <prynka.gsh@gmail.com>
2025-01-08 15:11:42 +0530

    thirsty branch changes

--- hungry.py
+++ hungry.py
@@ -1,9 +1,6 @@
 hungry=input("Are you hungry?")
 if hungry == "yes":
     print("Eat Biriyani!!")
-    print("Eat Pizza")
-    print("eat snacks")
-    print("eat samosa")
 else:
     thirsty = input("are you thirsty?")
     if thirsty == "yes":
